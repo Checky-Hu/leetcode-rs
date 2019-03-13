8bf6c365d69ce999034ce557ae9207351f4900f0
Checky-Hu <checkywho@gmail.com>
2019-03-13 11:49:03 -0400

    finish rotate image

--- rotate-image/src/main.rs
+++ rotate-image/src/main.rs
@@ -6,6 +6,30 @@ struct Solution {
 
 impl Solution {
     pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let len = matrix.len();
+	if len == 0 {
+	    return;
+	}
+
+	let mut index: usize = 0;
+	let mut cur_len: usize = len - 1;
+	loop {
+	    let mut offset: usize = 0;
+	    while offset < cur_len {
+	        let tmp: i32 = matrix[index][index + offset];
+		matrix[index][index + offset] = matrix[index + cur_len - offset][index];
+		matrix[index + cur_len - offset][index] = matrix[index + cur_len][index + cur_len - offset];
+		matrix[index + cur_len][index + cur_len - offset] = matrix[index + offset][index + cur_len];
+		matrix[index + offset][index + cur_len] = tmp;
+		offset += 1;
+	    }
+	    index += 1;
+	    if cur_len <= 2 {
+	        break;
+	    } else {
+	        cur_len -= 2;
+	    }
+	}
     }
 }
 
