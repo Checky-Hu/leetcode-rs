ff908053b20084c89b2083fa93a2f2b473af4830
Checky-Hu <checky_hu@qq.com>
2019-07-03 05:52:46 +0000

    optimize baseball game

--- 14-50/baseball-game/src/main.rs
+++ 14-50/baseball-game/src/main.rs
@@ -7,17 +7,20 @@ struct Solution {
 impl Solution {
     pub fn cal_points(ops: Vec<String>) -> i32 {
         let mut points: Vec<i32> = Vec::new();
+	let mut len: usize = 0;
 	for s in ops {
 	    if s == "+" {
-	        let len: usize = points.len();
 	        points.push(points[len - 2] + points[len - 1]);
+		len += 1;
 	    } else if s == "D" {
-	        let len: usize = points.len();
 	        points.push(points[len - 1] * 2);
+		len += 1;
 	    } else if s == "C" {
 	        points.pop();
+		len -= 1;
 	    } else {
 	        points.push(i32::from_str(&s).expect("Error parse."));
+		len += 1;
 	    }
 	}
 	let mut result: i32 = 0;
