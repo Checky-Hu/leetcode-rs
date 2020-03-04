bd5f050c4afeb6932253d6d0115d6c811cd87574
Checky-Hu <checkywho@gmail.com>
2020-03-04 19:34:46 +0800

    format binary search code

--- base/binary-search/src/lib.rs
+++ base/binary-search/src/lib.rs
@@ -7,49 +7,45 @@ mod tests {
 }
 
 pub mod bsi32 {
-    pub fn binary_search(nums: &Vec<i32>, left: usize, right: usize, target: i32) -> i32 {
-	let mut i: usize = left;
-	let mut j: usize = right;
-	while i <= j {
-	    let index: usize = i + (j - i) / 2;
-	    if nums[index] == target {
-	        return index as i32
-	    } else if nums[index] < target {
-	        i = index + 1;
-	    } else {
-	        if index >= 1 {
-	            j = index - 1;
-		} else {
-		    break;
-		}
-	    }
-	}
-	-1
+    pub fn binary_search(nums: &[i32], left: usize, right: usize, target: i32) -> i32 {
+        let mut i: usize = left;
+        let mut j: usize = right;
+        while i <= j {
+            let index: usize = i + (j - i) / 2;
+            if nums[index] == target {
+                return index as i32;
+            } else if nums[index] < target {
+                i = index + 1;
+            } else if index >= 1 {
+                j = index - 1;
+            } else {
+                break;
+            }
+        }
+        -1
     }
 
-    pub fn binary_search_for_pos(nums: &Vec<i32>, left: usize, right: usize, target: i32) -> i32 {
-	if nums[left] > target {
-	    -1
-	} else if nums[right] < target {
-	    -2
-	} else {
+    pub fn binary_search_for_pos(nums: &[i32], left: usize, right: usize, target: i32) -> i32 {
+        if nums[left] > target {
+            -1
+        } else if nums[right] < target {
+            -2
+        } else {
             let mut i: usize = left;
-	    let mut j: usize = right;
-	    while i <= j {
-	        let index: usize = i + (j - i) / 2;
-	        if nums[index] == target {
-	            return index as i32
-	        } else if nums[index] < target {
-	            i = index + 1;
-	        } else {
-	            if index >= 1 {
-	                j = index - 1;
-		    } else {
-		        break;
-		    }
-	        }
-	    }
-	    j as i32
-	}
+            let mut j: usize = right;
+            while i <= j {
+                let index: usize = i + (j - i) / 2;
+                if nums[index] == target {
+                    return index as i32;
+                } else if nums[index] < target {
+                    i = index + 1;
+                } else if index >= 1 {
+                    j = index - 1;
+                } else {
+                    break;
+                }
+            }
+            j as i32
+        }
     }
 }
