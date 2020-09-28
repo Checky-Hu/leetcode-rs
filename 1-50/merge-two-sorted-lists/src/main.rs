f8e8cbb9bb3c7a84a4ea5d8e91974d77a6e57979
Checky-Hu <checkywho@gmail.com>
2020-09-28 22:44:51 +0800

    finish merge two sorted lists

--- 1-50/merge-two-sorted-lists/src/main.rs
+++ 1-50/merge-two-sorted-lists/src/main.rs
@@ -10,77 +10,73 @@ pub struct ListNode {
 impl ListNode {
     #[inline]
     pub fn new(v: i32) -> Self {
-        ListNode {
-	    val: v,
-	    next: None,
-	}
+        ListNode { val: v, next: None }
     }
 }
 
-struct Solution {
-}
+struct Solution {}
 
 impl Solution {
-    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        l1
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let (mut l1_mut, mut l2_mut) = (l1, l2);
+        let mut header: Box<ListNode> = Box::new(ListNode::new(0));
+        let mut result: &mut Box<ListNode> = &mut header;
+        while let (Some(x), Some(y)) = (l1_mut.as_ref(), l2_mut.as_ref()) {
+            if x.val < y.val {
+                result.next = l1_mut;
+                result = result.next.as_mut().unwrap();
+                l1_mut = result.next.take();
+            } else {
+                result.next = l2_mut;
+                result = result.next.as_mut().unwrap();
+                l2_mut = result.next.take();
+            }
+        }
+        result.next = if l1_mut.is_some() { l1_mut } else { l2_mut };
+        header.next
     }
 }
 
 fn main() {
-    let mut strs: [String; 2] = ["".to_string(), "".to_string()];
+    let mut ret: usize = 0;
+    let mut len: usize = 0;
+    let mut count: usize = 0;
+    let mut l1: Box<ListNode> = Box::new(ListNode::new(0));
+    let mut l1_mut: &mut Box<ListNode> = &mut l1;
+    let mut l2: Box<ListNode> = Box::new(ListNode::new(0));
+    let mut l2_mut: &mut Box<ListNode> = &mut l2;
     for (index, arg) in env::args().enumerate() {
-        if index == 1 {
-            strs[0] = arg;
-	} else if index == 2 {
-            strs[1] = arg;
-	    break;
-	} else {
-	    continue;
-	}
+        match index {
+            0 => (),
+            1 => len = usize::from_str(&arg).expect("Error parse."),
+            _ => {
+                ret += 1;
+                let val: i32 = i32::from_str(&arg).expect("Error parse.");
+                if count == len {
+                    l2_mut.next = Some(Box::new(ListNode::new(val)));
+                    l2_mut = l2_mut.next.as_mut().unwrap();
+                } else {
+                    l1_mut.next = Some(Box::new(ListNode::new(val)));
+                    l1_mut = l1_mut.next.as_mut().unwrap();
+                    count += 1;
+                }
+            }
+        }
     }
 
-    if 0 == strs[1].len() {
-        println!("Require at least two parameter.");
-	return;
+    if ret == 0 {
+        println!("Require at least (1 + arg1) parameters.");
+        return;
     }
 
-    let mut vecs: [Vec<&str>; 2]= [vec!(), vec!()];
-    let mut index: usize = 0;
-    let mut ls1: Option<Box<ListNode>> = None;
-    let mut ls2: Option<Box<ListNode>> = None;
-    while index < vecs.len() {
-        vecs[index] = strs[index].split(',').collect();
-
-	let mut tmp_i: usize = 0;
-	let mut tmp_list: &Option<Box<ListNode>> = match index {
-	    0 => &mut ls1,
-	    1 => &mut ls2,
-	    _ => &None,
-	};
-        for s in &vecs[index] {
-	    let tmp_node = ListNode::new(i32::from_str(&s).expect("Parse Error"));
-	    match tmp_i {
-	        0 => *tmp_list = Some(Box::new(tmp_node)),
-		_ => {
-		    match tmp_list {
-		        Some(ref p) => {
-			    p.next = Some(Box::new(tmp_node));
-			    *tmp_list = p.next;
-			},
-		        None => (),
-		    }
-		},
-	    }
-	    tmp_i += 1;
-	}
-	index += 1;
+    let result: Option<Box<ListNode>> = Solution::merge_two_lists(l1.next, l2.next);
+    let mut t: Option<&Box<ListNode>> = result.as_ref();
+    while let Some(x) = t {
+        print!("{} ", x.val);
+        t = x.next.as_ref();
     }
-
-    let mut result = Solution::merge_two_lists(ls1, ls2);
-    print!("List: ");
-    while result != None {
-        print!("{} ", result.unwrap().val);
-	result = result.unwrap().next;
-    }
-    print!("\n");
+    println!();
 }
