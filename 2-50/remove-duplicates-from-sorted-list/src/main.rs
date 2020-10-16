776a8dc925e0d57af7e25ee569cda4247148e095
Checky-Hu <checkywho@gmail.com>
2020-10-16 15:08:58 +0800

    finish remove duplicates from sorted list

---
+++ 2-50/remove-duplicates-from-sorted-list/src/main.rs
@@ -0,0 +1,67 @@
+use std::env;
+use std::str::FromStr;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(v: i32) -> Self {
+        ListNode { val: v, next: None }
+    }
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head_mut: Option<Box<ListNode>> = head;
+        let mut result: Option<&mut Box<ListNode>> = head_mut.as_mut();
+        while let Some(mut x) = result {
+            if let Some(y) = x.next.as_mut() {
+                if x.val == y.val {
+                    x.next = y.next.take();
+                    result = Some(x);
+                } else {
+                    result = x.next.as_mut();
+                }
+            } else {
+                break;
+            }
+        }
+        head_mut
+    }
+}
+
+fn main() {
+    let mut ret: usize = 0;
+    let mut head: Box<ListNode> = Box::new(ListNode::new(0));
+    let mut head_mut: &mut Box<ListNode> = &mut head;
+    for (index, arg) in env::args().enumerate() {
+        match index {
+            0 => (),
+            _ => {
+                ret += 1;
+                let val: i32 = i32::from_str(&arg).expect("Error parse.");
+                head_mut.next = Some(Box::new(ListNode::new(val)));
+                head_mut = head_mut.next.as_mut().unwrap();
+            }
+        }
+    }
+
+    if ret == 0 {
+        println!("Require at least 1 parameter.");
+        return;
+    }
+
+    let result: Option<Box<ListNode>> = Solution::delete_duplicates(head.next);
+    let mut t: Option<&Box<ListNode>> = result.as_ref();
+    while let Some(x) = t {
+        print!("{} ", x.val);
+        t = x.next.as_ref();
+    }
+    println!();
+}
