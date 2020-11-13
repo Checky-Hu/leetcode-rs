d208fc838cfaa62afd09cf9534df58f816deccca
Checky-Hu <checkywho@gmail.com>
2020-11-13 21:38:37 +0800

    finish repeated dna sequences

--- 4-50/repeated-dna-sequences/src/main.rs
+++ 4-50/repeated-dna-sequences/src/main.rs
@@ -1,10 +1,35 @@
+use std::collections::HashMap;
 use std::env;
 
 struct Solution {}
 
 impl Solution {
     pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
-        Vec::new()
+        let mut result: Vec<String> = Vec::new();
+        let mut map: HashMap<String, i32> = HashMap::new();
+        let mut current: String = String::new();
+        for c in s.chars() {
+            if current.len() == 10 {
+                current.remove(0);
+                current.push(c);
+            } else {
+                current.push(c);
+            }
+            if current.len() == 10 {
+                match map.get_mut(&current) {
+                    Some(x) => *x += 1,
+                    None => {
+                        map.insert(current.clone(), 1);
+                    }
+                }
+            }
+        }
+        for (k, v) in map.iter() {
+            if *v > 1 {
+                result.push(k.clone());
+            }
+        }
+        result
     }
 }
 
